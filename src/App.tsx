0023d0fb3fe05d426f42512dd83a48f1db73b90b
Foo Bar <foo@bar.com>
2019-02-09 21:56:37 -0800

    Got todo item adding added
    This change allows users to add todo items.

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,137 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, ChangeEvent, KeyboardEvent } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
+interface TodoItemData {
+  id: string;
+
+  text: string;
+
+  selected: boolean;
+}
+
+interface TodoList {
+  currentText: string;
+
+  todoItems: TodoItemData[];
+}
+
+interface AppProps {}
+
+class App extends Component<AppProps, TodoList> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      currentText: "",
+      todoItems: [
+        {
+          id: "hard-coded-1",
+          text: "foo bar",
+          selected: false
+        },
+        {
+          id: "hard-coded-2",
+          text: "foo bar 2",
+          selected: false
+        }
+      ]
+    };
+  }
+
+  renderTodoItems = () => {
+    return this.state.todoItems.map(todoItem => {
+      return <TodoItem text={todoItem.text} />;
+    });
+  };
+
+  onHeaderCurrentTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ currentText: event.target.value });
+  };
+
+  onHeaderTextAccepted = () => {
+    const newTodoItem: TodoItemData = {
+      id: "hardcoded-id-for-new-items",
+      text: this.state.currentText,
+      selected: false
+    };
+
+    const newTodoItemArray = [...this.state.todoItems, newTodoItem];
+
+    this.setState({ todoItems: newTodoItemArray, currentText: "" });
+  };
+
   render() {
     return (
       <div className="App">
+        <TodoHeaderSection
+          currentText={this.state.currentText}
+          onEnterPressed={this.onHeaderTextAccepted}
+          onChange={this.onHeaderCurrentTextChange}
+        />
 
-        <TodoHeaderSection />
-
-        <TodoItem text='foo 1' />
-        <TodoItem text='foo 1' />
-        <TodoItem text='foo 1' />
-        <TodoItem text='foo 1' />
+        {this.renderTodoItems()}
 
         <TodoFooterSection />
-
       </div>
     );
   }
 }
 
+interface TodoHeaderSectionProps {
+  currentText: string;
+  onChange(event: ChangeEvent<HTMLInputElement>): void;
+  onEnterPressed(): void;
+}
 
-class TodoHeaderSection extends Component {
-  render(){
+class TodoHeaderSection extends Component<TodoHeaderSectionProps> {
+  handleKeypress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      this.props.onEnterPressed();
+    }
+  };
+
+  render() {
     return (
-        <div className='todo-header-section'>
-          <span>\/</span>
-          <input placeholder='What needs to be done?'></input>
-        </div>
-    )
+      <div className="todo-header-section">
+        <span>\/</span>
+        <input
+          onChange={this.props.onChange}
+          placeholder="What needs to be done?"
+          value={this.props.currentText}
+          onKeyPress={this.handleKeypress}
+        />
+      </div>
+    );
   }
 }
 
-
 interface TodoItemProps {
-  text: string
+  text: string;
 }
 
 class TodoItem extends Component<TodoItemProps> {
-  render(){
+  render() {
     return (
-        <div className='todo-item'>
-          <input type='checkbox' />
-          <span> {this.props.text} </span>
-        </div>
-    )
+      <div className="todo-item">
+        <input type="checkbox" />
+        <span> {this.props.text} </span>
+      </div>
+    );
   }
 }
-
 
 class TodoFooterSection extends Component {
-  render(){
+  render() {
     return (
-        <div className='todo-footer-section'>
-          <span>n items</span>
+      <div className="todo-footer-section">
+        <span>n items</span>
 
-          <span>All</span>
-          <span>Active</span>
-          <span>Completed</span>
-        </div>
-    )
+        <span>All</span>
+        <span>Active</span>
+        <span>Completed</span>
+      </div>
+    );
   }
 }
-
-
 
 export default App;
